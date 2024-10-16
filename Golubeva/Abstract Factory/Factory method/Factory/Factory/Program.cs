a4081ef6c03a49b326139fd518b3a03f8aebf5ef
Евгения15 <102397560+golubevaEvgen@users.noreply.github.com>
2024-10-16 14:50:07 +1000

    1
    1 lab

---
+++ Golubeva/Abstract Factory/Factory method/Factory/Factory/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public interface INotification
+{
+    void Send(string message);
+}
+
+public class EmailNotification : INotification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Sending Email Notification: {message}");
+    }
+}
+
+public class SMSNotification : INotification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Sending SMS Notification: {message}");
+    }
+}
+
+public class PushNotification : INotification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Sending Push Notification: {message}");
+    }
+}
+
+public static class NotificationFactory
+{
+    public static INotification CreateNotification(string type)
+    {
+        switch (type.ToLower())
+        {
+            case "email":
+                return new EmailNotification();
+            case "sms":
+                return new SMSNotification();
+            case "push":
+                return new PushNotification();
+            default:
+                throw new ArgumentException("Invalid notification type");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        INotification notification = NotificationFactory.CreateNotification("email");
+        notification.Send("Hello, this is a test email!");
+
+        notification = NotificationFactory.CreateNotification("sms");
+        notification.Send("Hello, this is a test SMS!");
+
+        notification = NotificationFactory.CreateNotification("push");
+        notification.Send("Hello, this is a test push notification!");
+    }
+}
